91e0de3d22b1bd28880ff0fa98b4fe2e195946a7
Allan Bantilan <allan.bantilan@coreproc.com>
2026-04-06 23:54:25 +0800

    feat: add contact fields to admin profile

---
+++ app/Filament/Pages/EditProfile.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use Filament\Auth\Pages\EditProfile as BaseEditProfile;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Schema;
+
+class EditProfile extends BaseEditProfile
+{
+    public function form(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                $this->getNameFormComponent(),
+                $this->getEmailFormComponent(),
+                TextInput::make('mobile_number')
+                    ->label('Mobile Number')
+                    ->maxLength(30)
+                    ->tel(),
+                TextInput::make('facebook_url')
+                    ->label('Facebook URL')
+                    ->url()
+                    ->maxLength(255),
+                TextInput::make('instagram_url')
+                    ->label('Instagram URL')
+                    ->url()
+                    ->maxLength(255),
+                $this->getPasswordFormComponent(),
+                $this->getPasswordConfirmationFormComponent(),
+                $this->getCurrentPasswordFormComponent(),
+            ]);
+    }
+}
